de0b6ae8edea603967536111ffe93f96b75b96aa
buchi1002 <peacek@icloud.com>
2022-08-17 20:08:50 +0900

    add ABC 257

--- ABC/257/e.py
+++ ABC/257/e.py
@@ -1,1 +1,10 @@
-N 
+def main():
+    N = int(input())
+    Cs = map(int,input().split())
+    dp = [[0]*N for i in range()]
+
+    for i in range(N):
+        for j in range(9):
+            dp[j][k] =
+if __name__ == '__main__':
+    main()
